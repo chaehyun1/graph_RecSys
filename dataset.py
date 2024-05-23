2c071c1c13da81dab337e1bb62640c07cd03d5d8
김채현 <108905986+chaehyun1@users.noreply.github.com>
2024-05-23 15:32:37 +0900

    Add files via upload

--- dataset.py
+++ dataset.py
@@ -3,7 +3,8 @@ import scipy.sparse as sp
 import numpy as np
 import torch
 import os
-
+from datautil import load_rating_file_to_matrix, load_rating_file_to_list, load_negative_file, \
+    load_group_member_to_dict, build_hyper_graph, build_group_graph, build_light_gcn_graph
 
 class Dataset:
     def __init__(self, path):
@@ -12,150 +13,140 @@ class Dataset:
         self.group_test_file = path + '/groupRatingTest.txt'
         self.train_file = path + '/userRatingTrain.txt'
         self.test_file = path + '/userRatingTest.txt'
+        self.group_member_file = path + '/groupMember.txt'
+        self.group_negative_file = path + '/groupRatingNegative.txt'
+        self.user_negative_file = path + '/userRatingNegative.txt'
+        
+        self.user_train_matrix = load_rating_file_to_matrix(self.train_file)
+        self.user_test_matrix = load_rating_file_to_matrix(self.test_file)
+        self.user_test_ratings = load_rating_file_to_list(self.test_file)
+        self.user_test_negatives = load_negative_file(self.user_negative_file)
+        self.num_users, self.num_items = self.user_train_matrix.shape
+        
+        print(f"UserItem: {self.user_train_matrix.shape} with {len(self.user_train_matrix.keys())} "
+              f"interactions, sparsity: {(1-len(self.user_train_matrix.keys()) / self.num_users / self.num_items):.5f}")
+    
+        # Group data
+        self.group_train_matrix = load_rating_file_to_matrix(self.group_train_file) # (290, 7710)
+        self.group_test_matrix = load_rating_file_to_matrix(self.group_test_file)
+        self.group_test_ratings = load_rating_file_to_list(self.group_test_file)
+        self.group_test_negatives = load_negative_file(self.group_negative_file)
+        self.num_groups, self.num_group_net_items = self.group_train_matrix.shape
+        self.group_member_dict = load_group_member_to_dict(self.group_member_file)
+
+        print(f"GroupItem: {self.group_train_matrix.shape} with {len(self.group_train_matrix.keys())} interactions, spa"
+              f"rsity: {(1-len(self.group_train_matrix.keys()) / self.num_groups / self.group_train_matrix.shape[1]):.5f}")
 
     def datasetToDataFrame(self, file_train, user=True):
         data = []
         with open(file_train, 'r') as file:
             for line in file:
-                line = line.strip().split()  
-                data.append([int(x) for x in line])  
+                line = line.strip().split()
+                if len(line) == 2:
+                    line.append(1)  # rating 값이 없을 때 1로 채움
+                data.append([int(x) for x in line])
 
         if user:
             df = pd.DataFrame(data, columns=['user_id', 'item_id', 'rating'])
         else:
             df = pd.DataFrame(data, columns=['group_id', 'item_id', 'rating'])
-        df['rating'] = 1
-        return df 
+        return df
+
+    def loadGroupMembers(self):
+        group_members = {}
+        with open(self.group_member_file, 'r') as file:
+            for line in file:
+                line = line.strip().split()
+                group_id = int(line[0])
+                member_ids = [int(x) for x in line[1].split(',')]
+                for member_id in member_ids:
+                    group_members[member_id] = group_id
+        return group_members
+    
+    def load_negative_file(self, filename):
+        """Return **List** format negative files"""
+        negative_list = []
+
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+
+            for line in lines:
+                negatives = line.split()[1:]
+                negatives = [int(neg_item) for neg_item in negatives]
+                negative_list.append(negatives)
+        return negative_list
     
     def scale_sparse_tensor(self, tensor):
-        # values를 0~1로 스케일링
         max_value = tensor._values().max().item()
         min_value = tensor._values().min().item()
         scaled_values = (tensor._values() - min_value) / (max_value - min_value)
-
-        # 새로운 스케일링된 sparse tensor 생성
         scaled_tensor = torch.sparse_coo_tensor(tensor._indices(), scaled_values, tensor.size())
-
         return scaled_tensor
+
+    def calculate_similarity(self, group_tensor, user_tensor):
+        group_norm = torch.norm(group_tensor, p=2, dim=1, keepdim=True)
+        user_norm = torch.norm(user_tensor, p=2, dim=1, keepdim=True)
+        similarity = torch.mm(group_tensor, user_tensor.t()) / (group_norm * user_norm.t())
+        similarity[torch.isnan(similarity)] = 0
+        return similarity
     
+
     def getDataset(self):
         df_group_train = self.datasetToDataFrame(self.group_train_file, False)
-        df_group_test = self.datasetToDataFrame(self.group_test_file, False)
         df_user_train = self.datasetToDataFrame(self.train_file)
-        df_user_test = self.datasetToDataFrame(self.test_file)
 
-        # 문제점: group에서의 item이 user에서의 item에 없는 경우 발생
-        # 해결 
-        unique_group_item = df_group_train['item_id'].unique()
-        unique_user_item = df_user_train['item_id'].unique()
-        group_not_in_user = set(unique_group_item) - set(unique_user_item) # 두 집합의 차집합을 계산하여 각 데이터프레임에서 겹치지 않는 ID 찾기
-        user_not_in_group = set(unique_user_item) - set(unique_group_item)
-        df_group_train = df_group_train[~df_group_train['item_id'].isin(group_not_in_user)]
-        df_user_train = df_user_train[~df_user_train['item_id'].isin(user_not_in_group)]
-        
-        unique_group_item = df_group_test['item_id'].unique()
-        unique_user_item = df_user_test['item_id'].unique()
-        group_not_in_user = set(unique_group_item) - set(unique_user_item)
-        user_not_in_group = set(unique_user_item) - set(unique_group_item)
-        df_group_test = df_group_test[~df_group_test['item_id'].isin(group_not_in_user)]
-        df_user_test = df_user_test[~df_user_test['item_id'].isin(user_not_in_group)]
-
-        # sort: unique groups, items, users
         train_groups = df_group_train['group_id'].unique()
-        train_group_items = df_group_train['item_id'].unique()
-        test_groups = df_group_test['group_id'].unique()
-        test_group_items = df_group_test['item_id'].unique()
         train_users = df_user_train['user_id'].unique()
-        train_user_items = df_user_train['item_id'].unique()
-        test_users = df_user_test['user_id'].unique()
-        test_user_items = df_user_test['item_id'].unique()
 
-        # dictionary for group, item, user
-        train_group_to_index = {group: index for index, group in enumerate(train_groups)}
-        train_group_item_to_index = {item: index for index, item in enumerate(train_group_items)}   
-        test_group_to_index = {group: index for index, group in enumerate(test_groups)}
-        test_group_item_to_index = {item: index for index, item in enumerate(test_group_items)} 
+        train_group_to_index = {group: index for index, group in enumerate(train_groups)}  
         train_user_to_index = {user: index for index, user in enumerate(train_users)}
-        train_user_item_to_index = {item: index for index, item in enumerate(train_user_items)} 
-        test_user_to_index = {user: index for index, user in enumerate(test_users)}
-        test_user_item_to_index = {item: index for index, item in enumerate(test_user_items)} 
 
-        # number of groups, items, users
-        train_num_groups = len(train_groups)
-        train_group_num_items = len(train_group_items) # 7686 -> 7057
-        test_num_groups = len(test_groups)
-        test_group_num_items = len(test_group_items)
-        train_num_users = len(train_users)
-        train_user_num_items = len(train_user_items) # 7057
-        test_num_users = len(test_users)
-        test_user_num_items = len(test_user_items)
+        train_group_matrix = self.group_train_matrix.tocsr()
+        test_group_matrix = self.group_test_matrix.tocsr()
+        train_user_matrix = self.user_train_matrix.tocsr()
+        test_user_matrix = self.user_test_matrix.tocsr()
 
-        # create sparse matrix
-        train_group_sparse_matrix = sp.lil_matrix((train_num_groups, train_group_num_items))
-        test_group_sparse_matrix = sp.lil_matrix((test_num_groups, test_group_num_items))
-        train_user_sparse_matrix = sp.lil_matrix((train_num_users, train_user_num_items))
-        test_user_sparse_matrix = sp.lil_matrix((test_num_users, test_user_num_items))
+        train_group_coo_matrix = train_group_matrix.tocoo()
+        test_group_coo_matrix = test_group_matrix.tocoo()
+        train_user_coo_matrix = train_user_matrix.tocoo()
+        test_user_coo_matrix = test_user_matrix.tocoo()
 
-        # fill sparse matrix
-        for _, row in df_group_train.iterrows():
-            group_index = train_group_to_index[row['group_id']]
-            item_index = train_group_item_to_index[row['item_id']]
-            train_group_sparse_matrix[group_index, item_index] = row['rating']
-
-        for _, row in df_group_test.iterrows():
-            group_index = test_group_to_index[row['group_id']]
-            item_index = test_group_item_to_index[row['item_id']]
-            test_group_sparse_matrix[group_index, item_index] = row['rating']
-
-        for _, row in df_user_train.iterrows():
-            user_index = train_user_to_index[row['user_id']]
-            item_index = train_user_item_to_index[row['item_id']]
-            train_user_sparse_matrix[user_index, item_index] = row['rating']
-
-        for _, row in df_user_test.iterrows():
-            user_index = test_user_to_index[row['user_id']]
-            item_index = test_user_item_to_index[row['item_id']]
-            test_user_sparse_matrix[user_index, item_index] = row['rating']
-
-        # 희소 행렬을 csr_matrix로 변환
-        train_group_sparse_matrix = train_group_sparse_matrix.tocsr()
-        test_group_sparse_matrix = test_group_sparse_matrix.tocsr()
-        train_user_sparse_matrix = train_user_sparse_matrix.tocsr()
-        test_user_sparse_matrix = test_user_sparse_matrix.tocsr()
-
-        train_group_csr_matrix = train_group_sparse_matrix
-        test_group_csr_matrix = test_group_sparse_matrix
-        train_user_csr_matrix = train_user_sparse_matrix
-        test_user_csr_matrix = test_user_sparse_matrix
-
-        # Convert CSR matrix to COO format (Coordinate List)
-        train_group_coo_matrix = train_group_csr_matrix.tocoo()
-        test_group_coo_matrix = test_group_csr_matrix.tocoo()
-        train_user_coo_matrix = train_user_csr_matrix.tocoo()
-        test_user_coo_matrix = test_user_csr_matrix.tocoo()
-
-        # Convert COO matrix to PyTorch sparse tensor
         train_group_data = torch.FloatTensor(train_group_coo_matrix.data)
         test_group_data = torch.FloatTensor(test_group_coo_matrix.data)
         train_user_data = torch.FloatTensor(train_user_coo_matrix.data)
         test_user_data = torch.FloatTensor(test_user_coo_matrix.data)
 
-        # Convert COO matrix to PyTorch LongTensor
         train_group_indices = torch.LongTensor(np.vstack((train_group_coo_matrix.row, train_group_coo_matrix.col)))
         test_group_indices = torch.LongTensor(np.vstack((test_group_coo_matrix.row, test_group_coo_matrix.col)))
         train_user_indices = torch.LongTensor(np.vstack((train_user_coo_matrix.row, train_user_coo_matrix.col)))
         test_user_indices = torch.LongTensor(np.vstack((test_user_coo_matrix.row, test_user_coo_matrix.col)))
 
-        # Convert COO matrix to PyTorch sparse tensor
         train_group_result = torch.sparse_coo_tensor(train_group_indices, train_group_data, torch.Size(train_group_coo_matrix.shape))
         test_group_result = torch.sparse_coo_tensor(test_group_indices, test_group_data, torch.Size(test_group_coo_matrix.shape))
         train_user_result = torch.sparse_coo_tensor(train_user_indices, train_user_data, torch.Size(train_user_coo_matrix.shape))
         test_user_result = torch.sparse_coo_tensor(test_user_indices, test_user_data, torch.Size(test_user_coo_matrix.shape))
+        
+        group_members = self.loadGroupMembers()
 
-        #train_group_result = self.scale_sparse_tensor(train_group_result)
-        #test_group_result = self.scale_sparse_tensor(test_group_result)
-        #train_user_result = self.scale_sparse_tensor(train_user_result)
-        #test_user_result = self.scale_sparse_tensor(test_user_result)
+        num_users = train_user_result.shape[0]
+        user_group_similarity = torch.zeros(num_users)
 
-        return train_group_result, test_group_result, train_user_result, test_user_result
+        for user_id, group_id in group_members.items():
+            if group_id in train_group_to_index and user_id in train_user_to_index:
+                group_index = train_group_to_index[group_id]
+                user_index = train_user_to_index[user_id]
+                group_vector = train_group_result[group_index].to_dense()
+                user_vector = train_user_result[user_index].to_dense()
 
+                similarity = self.calculate_similarity(group_vector.unsqueeze(0), user_vector.unsqueeze(0))
+                user_group_similarity[user_index] = similarity
+                
+        return train_group_result, test_group_result, train_user_result, test_user_result, user_group_similarity, self.load_negative_file(self.group_negative_file), self.load_negative_file(self.user_negative_file)
+
+
+# if __name__ == '__main__':
+#     # load dataset
+#     dataset = 'CAMRa2011'
+#     path = os.getcwd()  + f'/data/{dataset}/'
+#     data = Dataset(path) 
+#     R_tr_g, R_ts_g, R_tr_u, R_ts_u, C, g_neg, u_neg = data.getDataset()
